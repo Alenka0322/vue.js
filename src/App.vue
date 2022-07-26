fefce9601d0265d2b738a0abd4ab7dbcdf2b9e13
Alenka <alenkaandrievskaya_4@iclou.com>
2022-07-26 11:00:28 +0500

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,109 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer   v-model="drawer" app>
+    
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            INFORMATION
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            All that is needed
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="primari"
+      dark
+      src="graz-g69b2ef0cb_1920.jpg"
+      prominent
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(55,236,186,.1), rgba(25,32,72,.2)"
+         
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Lorem ips</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+        <v-text-field
+        @focus="searchClosed = false"
+        @blur="searchClosed = true"
+        v-model="search"
+            placeholder="Search"
+            prepend-inner-icon="mdi-magnify"
+            class="Lorem-ips mt-1 "
+            :class="{ 'closed' : searchClosed && !search }"
+           
+            filled
+            dense
+            clearable
+          ></v-text-field>
+    
+
+     
+    </v-app-bar>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+  export default {
+    data: () => ({ drawer: null,
+     items: [
+          { title: 'Block', icon: 'mdi-view-dashboard', to:'/' },
+          { title: 'Sign up', icon: 'mdi-account-circle', to:'/about' },
+        ],
+        search: null,
+        searchClosed: true,
+  
+    }),
   }
-}
+</script>
+
+
+<style lang="sass">
+.v-input.Lorem-ips
+  transition: max-width 0.3s
+.v-input__slot
+  cursor: pointer !important
+  &:before, &:after
+  border-color: transparent !important
+.v-input.closed
+  max-width: 45px
+  .v-input__slot
+    background: transparent imo !important
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,50 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <v-app>
+   <v-card width="400" class="mx-auto mt-5">
+    <v-card-title>
+     <h1>Login</h1>
+    </v-card-title>
+
+    <v-card-text>
+    <v-form>
+    <v-text-field label="Username"
+    prepend-icon="mdi-account-circle"
+    />
+    <v-text-field
+     :type="showPassword ? 'text' : 'password'"
+     label="Password"
+     prepend-icon="mdi-lock"
+     :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+     @click:append="showPassword = !showPassword"
+    />
+    </v-form>
+    </v-card-text>
+    <v-divider></v-divider>
+    <v-card-action class="btn pa-3" >
+        <v-btn  color="info">Registr</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="teal">Login</v-btn>
+    </v-card-action>
+
+   </v-card>
+    </v-app>
 </template>
+
+
+<script>
+export default {
+    name: 'About',
+    data: () => ({
+     showPassword: false
+    }),
+}
+</script>
+
+
+<style>
+.btn {
+    display: flex;
+    
+}
+
+</style>
